2c1d83443ade320fd19319946fd5a5f9af6cadb7
aidan-stritch <aidan.stritch@gmail.com>
2020-02-04 22:55:02 +0000

    corrected error to make sample data show on manage_users page

--- app.py
+++ app.py
@@ -3,24 +3,24 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-
+"""this is a comment"""
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] =  'Personal_Interest_Manager'
-app.config["MONGO_URI"] =  'mongodb+srv://PIM_Admin:PIM_R00T@interests-cluster-9djrk.mongodb.net/Personal_Interest_Manager?retryWrites=true&w=majority'
+app.config["MONGO_DBNAME"] = 'Interests_DB'
+app.config["MONGO_URI"] = 'mongodb+srv://PIM_Admin:PIM_R00T@interests-cluster-9djrk.mongodb.net/Interests_DB?retryWrites=true&w=majority'
 
 
 mongo = PyMongo(app)
 
-
 @app.route('/')
 @app.route('/manage_users')
 def manage_users():
-    return render_template('manage_users.html', user=mongo.db.Users.find())
-
+    user_name = mongo.db.Users.find({"First_Name": "Aidan"})
+    print(user_name)
+    return render_template('manage_users.html', Users=mongo.db.Users.find())
+    
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True) 
-
+            debug=True)
