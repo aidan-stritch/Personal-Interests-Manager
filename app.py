a17d2410741d74baf52fffb92949c2a404955b0d
aidan-stritch <aidan.stritch@gmail.com>
2020-02-07 18:02:17 +0000

    added template for base.html and set up inheritence for manage_users.html

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
+"""This area imports the neccessary libraries for this Python file"""
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-"""this is a comment"""
+"""Creating an instance of a Flask app and linking it to the MongoDB"""
 app = Flask(__name__)
-
 app.config["MONGO_DBNAME"] = 'Interests_DB'
 app.config["MONGO_URI"] = 'mongodb+srv://PIM_Admin:PIM_R00T@interests-cluster-9djrk.mongodb.net/Interests_DB?retryWrites=true&w=majority'
 
-
+"""Creates an instance of PyMongo with the app inside"""
 mongo = PyMongo(app)
 
+"""Creating our app routes"""
 @app.route('/')
 @app.route('/manage_users')
 def manage_users():
-    user_name = mongo.db.Users.find({"First_Name": "Aidan"})
-    print(user_name)
     return render_template('manage_users.html', Users=mongo.db.Users.find())
     
 
